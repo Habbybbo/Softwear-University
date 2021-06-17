b8037a9a834c76191ffe613872a6471ea43d6a5b
Yuliyan Andreev <habybbo@gmail.com>
2021-06-17 21:47:04 +0300

    C# Advanced exercises
    With some help

---
+++ DefiningClassesExercise/02.CreatingConstructors/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person maina = new Person();
+            Person bace = new Person(24);
+            Person chicoPencho = new Person("Chicho Pencho", 65);
+
+            Console.WriteLine($"{maina.Name} - {maina.Age}");
+            Console.WriteLine($"{bace.Name} - {bace.Age}");
+            Console.WriteLine($"{chicoPencho.Name} - {chicoPencho.Age}");
+        }
+    }
+}
---
+++ DefiningClassesExercise/06.SpeedRacing/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpeedRacing
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] carData = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string model = carData[0];
+                double fuelAmount = double.Parse(carData[1]);
+                double fuelConsumptionForOneKm = double.Parse(carData[2]);
+
+                Car currCar = new Car
+                {
+                    Model = model,
+                    FuelAmount = fuelAmount,
+                    FuelConsumptionPerKilometer = fuelConsumptionForOneKm
+                };
+
+                cars.Add(currCar);
+            }
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] commandData = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string model = commandData[1];
+                double distanceTraveled = double.Parse(commandData[2]);
+
+                Car car = cars.FirstOrDefault(c => c.Model == model);
+
+                bool isDrive = car.Drive(distanceTraveled);
+
+                if (isDrive == false)
+                {
+                    Console.WriteLine("Insufficient fuel for the drive");
+                }
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
