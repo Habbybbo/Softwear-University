a6a6211593f4a019f1c9b677c0980f569d32fadc
Yuliyan Andreev <habybbo@gmail.com>
2021-07-01 21:19:05 +0300

    C# OOP Exercises Lab

---
+++ InheritanceLab/RandomList/RandomList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public Random Rnd { get; set; }
+
+        public RandomList(Random rnd)
+        {
+            this.Rnd = rnd;
+        }
+
+        public string RemoveRandomElement()
+        {
+            int index = Rnd.Next(0, this.Count);
+            string str = this[index];
+            this.RemoveAt(index);
+            return str;
+
+        }
+    }
+}
