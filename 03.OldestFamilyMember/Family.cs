513aa66e35703e0f92970046c1d15784b00fd9e3
Yuliyan Andreev <habybbo@gmail.com>
2021-08-03 21:42:26 +0300

    C# OOP Exercises

---
+++ 03.OldestFamilyMember/Family.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+
+        private Dictionary<string, int> familyMembers;
+
+        public Family()
+        {
+            this.familyMembers = new Dictionary<string, int>();
+        }
+        public Family(string name, int age)
+        {
+
+        }
+
+        public void AddMember(Person member)
+        {
+            familyMembers.Add(member.Name, member.Age);
+
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestMember = new Person();
+
+            foreach (var person in familyMembers)
+            {
+                if (person.Value >= oldestMember.Age)
+                {
+                    oldestMember.Name = person.Key;
+                    oldestMember.Age = person.Value;
+                }
+            }
+
+            return oldestMember;
+        }
+    }
+}
---
+++ 03.OldestFamilyMember/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            Family familyMembers = new Family();
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] imputMembers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                Person member = new Person(imputMembers[0], int.Parse(imputMembers[1]));
+
+                familyMembers.AddMember(member);
+            }
+        }
+    }
+}
